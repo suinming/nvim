5d81d1a1580e69715ea5af7140bba14d3def0a3f
suinming <0223314338aa@gmail.com>
2026-04-27 00:49:41 +0800

    feat: update config for v0.12

--- init.lua
+++ init.lua
@@ -1,26 +1,233 @@
-require("config.lazy")
-require("config.lsp")
+-- ============================================================================
+-- Core
+-- ============================================================================
 
+local Config = {}
+_G.Config = Config
+
+local add = vim.pack.add
+
+add({ 'https://github.com/nvim-mini/mini.nvim' })
+local misc = require('mini.misc')
+
+Config.now = function(f) misc.safely('now', f) end
+Config.later = function(f) misc.safely('later', f) end
+
+-- Leader
+vim.g.mapleader = ' '
+
+-- Options
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.swapfile = false
+vim.opt.clipboard = 'unnamedplus'
 
--- clipboard
-vim.opt.clipboard:append({ "unnamedplus", "unnamed" })
-
--- source current file
-vim.keymap.set("n", "<space>x", "<cmd>source %<CR>")
-
--- highlight when yanking (copying) text
---  try it with `yap` in normal mode
---  see `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+-- Highlight yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function() vim.highlight.on_yank() end,
 })
+
+-- Fix formatoptions
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    vim.opt_local.formatoptions:remove({ 'c', 'o' })
+  end,
+})
+
+-- ============================================================================
+-- UI (colorscheme, lualine, icons)
+-- ============================================================================
+
+Config.now(function()
+  add({ 'https://github.com/rose-pine/neovim' })
+  vim.cmd('colorscheme rose-pine')
+end)
+
+Config.now(function()
+  add({
+    'https://github.com/nvim-tree/nvim-web-devicons',
+    'https://github.com/nvim-lualine/lualine.nvim',
+  })
+
+  local function lsp_detect()
+    local clients = vim.lsp.get_clients({ bufnr = 0 })
+    if #clients == 0 then return '' end
+    local names = {}
+    for _, c in ipairs(clients) do table.insert(names, c.name) end
+    return table.concat(names, ', ')
+  end
+
+  require('lualine').setup({
+    options = { theme = 'rose-pine' },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { { 'filename', path = 1 } },
+      lualine_x = { lsp_detect },
+      lualine_y = { 'selectioncount' },
+      lualine_z = { '%l/%L' },
+    },
+  })
+end)
+
+-- ============================================================================
+-- Editing (treesitter, completion, files, icons)
+-- ============================================================================
+
+Config.now(function()
+  add({
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
+  })
+
+  require('nvim-treesitter').setup({
+    ensure_installed = { 'lua', 'vimdoc', 'markdown' },
+    auto_install = true,
+    highlight = { enable = true },
+  })
+end)
+
+Config.now(function()
+  require('mini.completion').setup({})
+end)
+
+Config.now(function()
+  require('mini.files').setup({ windows = { preview = true } })
+end)
+
+vim.keymap.set('n', '<leader>e', function()
+  require('mini.files').open()
+end, { desc = 'Explorer' })
+
+Config.now(function()
+  require('mini.icons').setup({})
+end)
+
+-- ============================================================================
+-- LSP
+-- ============================================================================
+
+Config.now(function()
+  add({ 'https://github.com/neovim/nvim-lspconfig' })
+
+  -- https://github.com/vuejs/language-tools/wiki/Neovim
+  -- NOTE: install @vue/language-server
+  -- `npm install -g @vue/language-server`
+  -- check the path of vue_language_server_path
+  local vue_language_server_path =
+  '/home/suinming/.local/share/mise/installs/node/24.6.0/lib/node_modules/@vue/language-server'
+  local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+
+  local vue_plugin = {
+    name = '@vue/typescript-plugin',
+    location = vue_language_server_path,
+    languages = { 'vue' },
+    configNamespace = 'typescript',
+  }
+
+  -- NOTE: install typescript and typescript-language-server
+  -- `npm install -g typescript-language-server typescript`
+  local ts_ls_config = {
+    init_options = {
+      plugins = {
+        vue_plugin,
+      },
+    },
+    filetypes = tsserver_filetypes,
+  }
+
+  vim.lsp.config('ts_ls', ts_ls_config)
+
+  vim.lsp.enable({ 'lua_ls', 'ts_ls', 'vue_ls', 'jdtls', 'ruff', 'sqls' })
+end)
+
+-- Diagnostics
+Config.later(function()
+  vim.diagnostic.config({
+    virtual_text = { current_line = true },
+    underline = true,
+    update_in_insert = false,
+  })
+end)
+
+-- ============================================================================
+-- Formatting
+-- ============================================================================
+
+Config.later(function()
+  add({ 'https://github.com/stevearc/conform.nvim' })
+
+  require('conform').setup({
+    default_format_opts = { lsp_format = 'fallback' },
+  })
+end)
+
+vim.keymap.set('n', '<leader>lf', function()
+  require('conform').format({ async = true })
+end, { desc = 'Format buffer' })
+
+-- ============================================================================
+-- Telescope
+-- ============================================================================
+
+Config.now(function()
+  add({
+    'https://github.com/nvim-lua/plenary.nvim',
+    'https://github.com/nvim-telescope/telescope.nvim',
+    'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+  })
+
+  local telescope = require('telescope')
+  local builtin = require('telescope.builtin')
+
+  telescope.setup({
+    defaults = {
+      layout_strategy = 'horizontal',
+      layout_config = { horizontal = { preview_cutoff = 0 } },
+    },
+  })
+
+  vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+  vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = 'Live grep' })
+  vim.keymap.set('n', '<leader>fc', builtin.grep_string, { desc = 'Grep word' })
+  vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
+end)
+
+-- ============================================================================
+-- Tools (todo, dbee, leetcode)
+-- ============================================================================
+
+Config.now(function()
+  add({ 'https://github.com/folke/todo-comments.nvim' })
+  require('todo-comments').setup({})
+end)
+
+Config.now(function()
+  add({
+    'https://github.com/kndndrj/nvim-dbee',
+    'https://github.com/MunifTanjim/nui.nvim',
+  })
+
+  local dbee = require('dbee')
+
+  dbee.setup({
+    sources = {
+      require('dbee.sources').MemorySource:new({
+        {
+          id = 'tw_db',
+          name = 'tw_db',
+          type = 'sqlite',
+          url = vim.fn.expand('~/data/tw_db.sqlite'),
+        },
+      }),
+    },
+  })
+end)
+
+Config.now(function()
+  add({ 'https://github.com/kawre/leetcode.nvim' })
+  require('leetcode').setup({ lang = 'java' })
+end)
